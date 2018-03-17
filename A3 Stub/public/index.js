2ea8b7d5b4a90ce83750a6844f80e2f08721f304
vicmoh <vimoh1213@gmail.com>
2018-03-17 06:36:29 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -59,46 +59,6 @@ $(document).ready(function(){
         } // End if
     });
 
-    // C library API
-    const ffi = require('ffi');
-
-    //typedef
-    const ref = require("ref");
-    var GEDCOMobject = ref.types.void;
-    var GEDCOMobjectPtr = ref.refType(GEDCOMobject);
-
-    //create the lib for c
-    let parserLib = ffi.Library("./parser/bin/parser.so", {
-        // main writer gedcom
-        "createGEDCOMWrapper": [GEDCOMobjectPtr, ["string"]],
-        "writeGEDCOMWrapper": ["void", ["string", GEDCOMobjectPtr]],
-        //generation
-        "descToJSON": ["string", ["string", "string", "string", "int"]],
-        "anceToJSON": ["string", ["string", "string", "string", "int"]],
-        //indivvidual
-        "getIndiListJSON":["string", ["string"]],
-        "addIndiJSON": ["void", ["string", "string", "string"]]
-    });
-
-    function addIndividual(){
-        var uploadNameTest = "./uploads/writeTest.ged";
-    
-        console.log("before calling parser lib");
-        var fileNameTest = "./uploads/shakespeare.ged";
-        var objectTest = parserLib.createGEDCOMWrapper(fileNameTest);
-        console.log("middle calling parser lib");
-        var stringTest = parserLib.descToJSON(fileNameTest, "William", "Shakespeare", 3);
-        console.log(stringTest);
-        parserLib.writeGEDCOMWrapper(uploadNameTest, objectTest);
-        console.log("after calling parser lib");
-    
-        console.log("calling the create gedcom part 2");
-        console.log("testing to read the uploaded file");
-        var stringTest2 = parserLib.descToJSON(uploadNameTest, "William", "Shakespeare", 3);
-        console.log(stringTest2);
-        console.log("calling the parser PASSED");
-    }//end func
-
     //jquery for adding individual
     $('.addIndividual').on('click', function(event){
         //dec vars
