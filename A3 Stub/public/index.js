eedfbde89a628db48ecfe34fde598e5e4dfa6a99
vicmoh <vimoh1213@gmail.com>
2018-03-17 07:32:29 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -70,7 +70,7 @@ $(document).ready(function() {
         console.log("sex = " + sex);
         console.log("famSize = " + famSize);
         //parse the file to test
-        addIndividual();
+
         //clear text
         $('#addIndiFirstName').val(emptyString);
         $('#addIndiLastname').val(emptyString);
