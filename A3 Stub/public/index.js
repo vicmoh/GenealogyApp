f82bbe38e5029269e43efe074dc94a561fbe638c
vicmoh <vimoh1213@gmail.com>
2018-03-18 01:41:18 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -1,58 +1,61 @@
 // Put all onload AJAX calls here, and event listeners
 $(document).ready(function() {
     console.log("page has been loaded");
-    // // On page-load AJAX Example
-    // $.ajax({
-    //     type: 'get',            //Request type
-    //     dataType: 'json',       //Data type - we will use JSON for almost everything 
-    //     url: '/someendpoint',   //The server endpoint we are connecting to
-    //     success: function (data) {
-    //         /*  Do something with returned object
-    //             Note that what we get is an object, not a string, 
-    //             so we do not need to parse it on the server.
-    //             JavaScript really does handle JSONs seamlessly
-    //         */
-
-    //         //We write the object to the console to show that the request was successful
-    //         console.log(data); 
-    //     },
-    //     fail: function(error) {
-    //         // Non-200 return, do something with error
-    //         console.log(error); 
-    //     }
-    // });
 
     /*******************************************************************************
      * My ajax code 
      *******************************************************************************/
 
-    // //ajax get the list of file names
-    // $.ajax({
-    //     type: 'get',
-    //     dataType: 'json',
-    //     url: '/objects/listOfFileNames.json',   
-    //     success: function (data) {
-    //         console.log("ajax fileName are " + data);
-    //         var listOfFileNames = data;
-    //         //add file names to the selected
-    //         for(x = 0; x<listOfFileNames.length; x++){
-    //             currentFileName = "<option>"+listOfFileNames[x]+"</option>";
-    //             $("select").append(currentFileName);
-    //             console.log("listing all the file:");
-    //             console.log(currentFileName);
-    //         }//end for
-    //         location.reload();
-    //     },
-    //     fail: function(error) {
-    //         // Non-200 return, do something with error
-    //         console.log(error); 
-    //     }
-    // });
+    //ajax get the list of file names
+    $.ajax({
+        type: 'get',
+        dataType: 'json',
+        url: '/objects/listOfFileNames.json',   
+        success: function (data) {
+            console.log("ajax fileName are " + data);
+            var listOfFileNames = data;
+            //add file names to the selected
+            for(x = 0; x<listOfFileNames.length; x++){
+                currentFileName = "<option>"+listOfFileNames[x]+"</option>";
+                $("select").append(currentFileName);
+                console.log("listing all the file:");
+                console.log(currentFileName);
+            }//end for
+            location.reload();
+        },
+        fail: function(error) {
+            // Non-200 return, do something with error
+            console.log(error); 
+        }
+    });
 
     /*******************************************************************************
      * My jquery code 
      *******************************************************************************/
 
+    //refresh the page when clicked
+    $("*").on('click', function(event){
+        $.ajax({
+            type: 'get',
+            dataType: 'json',
+            url: '/objects/listOfFileNames.json',   
+            success: function (data) {
+                console.log("ajax fileName are " + data);
+                listOfFileNames = data;
+                //add file names to the selected
+                for(x = 0; x<listOfFileNames.length; x++){
+                    currentFileName = "<option>"+listOfFileNames[x]+"</option>";
+                    $("select").append(currentFileName);
+                    console.log("listing all the file:");
+                    console.log(currentFileName);
+                }//end for
+            },
+            fail: function(error) {
+                // Non-200 return, do something with error
+                console.log(error); 
+            }
+        });
+    });
     //smooth scrolling to setAnimateScroll
     $('.setAnimateScroll').on('click', function(event) {
         //make sure this.hash has a value before overriding default behavior
@@ -93,57 +96,5 @@ $(document).ready(function() {
         $('#addIndiLastname').val(emptyString);
         $('#addIndiSex').val(emptyString);
         $('#addIndiFamSize').val(emptyString);
-    });
-});
-
-$(document).ready(function() {
-    //dec vars
-    console.log("ajax loaded");
-    var listOfFileNames = [];
-    
-    //ajax get the list of file names
-    $.ajax({
-        type: 'get',
-        dataType: 'json',
-        url: '/objects/listOfFileNames.json',   
-        success: function (data) {
-            console.log("ajax fileName are " + data);
-            listOfFileNames = data;
-            //add file names to the selected
-            for(x = 0; x<listOfFileNames.length; x++){
-                currentFileName = "<option>"+listOfFileNames[x]+"</option>";
-                $("select").append(currentFileName);
-                console.log("listing all the file:");
-                console.log(currentFileName);
-            }//end for
-        },
-        fail: function(error) {
-            // Non-200 return, do something with error
-            console.log(error); 
-        }
-    });
-
-    //refresh the page when clicked
-    $("*").on('click', function(event){
-        $.ajax({
-            type: 'get',
-            dataType: 'json',
-            url: '/objects/listOfFileNames.json',   
-            success: function (data) {
-                console.log("ajax fileName are " + data);
-                listOfFileNames = data;
-                //add file names to the selected
-                for(x = 0; x<listOfFileNames.length; x++){
-                    currentFileName = "<option>"+listOfFileNames[x]+"</option>";
-                    $("select").append(currentFileName);
-                    console.log("listing all the file:");
-                    console.log(currentFileName);
-                }//end for
-            },
-            fail: function(error) {
-                // Non-200 return, do something with error
-                console.log(error); 
-            }
-        });
     });
 });
