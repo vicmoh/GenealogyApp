6b396df48e07c3ca95f73ac84051d6fe68aab17f
vicmoh <vimoh1213@gmail.com>
2018-03-18 01:10:15 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -1,25 +1,25 @@
 // Put all onload AJAX calls here, and event listeners
 $(document).ready(function() {
-    // On page-load AJAX Example
-    $.ajax({
-        type: 'get',            //Request type
-        dataType: 'json',       //Data type - we will use JSON for almost everything 
-        url: '/someendpoint',   //The server endpoint we are connecting to
-        success: function (data) {
-            /*  Do something with returned object
-                Note that what we get is an object, not a string, 
-                so we do not need to parse it on the server.
-                JavaScript really does handle JSONs seamlessly
-            */
+    // // On page-load AJAX Example
+    // $.ajax({
+    //     type: 'get',            //Request type
+    //     dataType: 'json',       //Data type - we will use JSON for almost everything 
+    //     url: '/someendpoint',   //The server endpoint we are connecting to
+    //     success: function (data) {
+    //         /*  Do something with returned object
+    //             Note that what we get is an object, not a string, 
+    //             so we do not need to parse it on the server.
+    //             JavaScript really does handle JSONs seamlessly
+    //         */
 
-            //We write the object to the console to show that the request was successful
-            console.log(data); 
-        },
-        fail: function(error) {
-            // Non-200 return, do something with error
-            console.log(error); 
-        }
-    });
+    //         //We write the object to the console to show that the request was successful
+    //         console.log(data); 
+    //     },
+    //     fail: function(error) {
+    //         // Non-200 return, do something with error
+    //         console.log(error); 
+    //     }
+    // });
 
     /*******************************************************************************
      * My ajax code 
