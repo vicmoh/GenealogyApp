75f216c75516f26e9822aa7b97fd47af5b7a8c2e
vicmoh <vimoh1213@gmail.com>
2018-03-18 01:15:43 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -40,6 +40,7 @@ $(document).ready(function() {
                 console.log("listing all the file:");
                 console.log(currentFileName);
             }//end for
+            location.reload();
         },
         fail: function(error) {
             // Non-200 return, do something with error
