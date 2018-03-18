9b394bf9e5d73e817bbf51f1fab9735cf7d80988
vicmoh <vimoh1213@gmail.com>
2018-03-18 00:51:10 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -35,16 +35,16 @@ $(document).ready(function() {
         success: function (data) {
             console.log("ajax fileName are " + data);
             listOfFileNames = data;
+            //add file names to the selected
+            for(x = 0; x<listOfFileNames.length; x++){
+                $('<select>').appendTo("<input>"+listOfFileNames[x]+"</input>");
+            }//end for
         },
         fail: function(error) {
             // Non-200 return, do something with error
             console.log(error); 
         }
     });
-    //add file names to the selected
-    for(x = 0; x<listOfFileNames.length; x++){
-        $('<select>').appendTo("<input>"+listOfFileNames[x]+"</input>");
-    }//end for
 
     /*******************************************************************************
      * My jquery code 
