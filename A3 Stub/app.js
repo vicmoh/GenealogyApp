47a991e1246e14702eead2d54b5b62d09926f2b1
vicmoh <vimoh1213@gmail.com>
2018-03-19 15:10:38 -0400

    asd

--- A3 Stub/app.js
+++ A3 Stub/app.js
@@ -242,11 +242,11 @@ app.get('/createGedcom', function(req , res){
     var file = req.query.fileName;
     var filePath = "./uploads/" + file + ".ged";
     console.log("filePath = " + filePath);
-    var subName = req.quary.subName;
+    var subNameGed = req.quary.subNameGed;
     var subAddress = req.quary.subAddress;
     var source = "Ancestry.com"
-    console.log("sub name: " + subName + ", sub address: " + subAddress);
-    var gedcomObject = parserLib.newGEDCOM(source, subName, subAddress);
+    console.log("sub name: " + subNameGed + ", sub address: " + subAddress);
+    var gedcomObject = parserLib.newGEDCOM(source, subNameGed, subAddress);
     parserLib.writeGEDCOMWrapper(filePath, gedcomObject);
     // var jsonString = parseLib.GEDCOMtoJSON(filePath);
     // console.log("json string of new gedcom: " + jsonString);
