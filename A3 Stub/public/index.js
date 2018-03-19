c61e6153084192c5a4828e347ef9d453d403dc1f
vicmoh <vimoh1213@gmail.com>
2018-03-19 00:14:55 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
     });
 
     //jquery for showing the indi
-    $('select').on('#close').change('click', function(event){
+    $('#gedcomFileSelection').change('click', function(event){
         console.log("calling ajax selection menu");
         var element = document.getElementById('fileSelection');
         var fileSelected = element.options[element.selectedIndex].text;
