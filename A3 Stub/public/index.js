76b90eabeae2267f7336eeaf887efe07edb93b97
vicmoh <vimoh1213@gmail.com>
2018-03-19 01:21:27 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -144,7 +144,7 @@ $(document).ready(function() {
     });
 
     //jquery for showing the indi
-    $('.addIndividual').change('click', function(event){
+    $('.searchDesc').change('click', function(event){
         //selection menu
         console.log("calling ajax selection menu");
         var element = document.getElementById('descFileSelection');
