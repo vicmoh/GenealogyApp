309067425199ab875e544cfba1ad698d61deda2e
vicmoh <vimoh1213@gmail.com>
2018-03-19 01:52:34 -0400

    asd

--- A3 Stub/app.js
+++ A3 Stub/app.js
@@ -201,7 +201,7 @@ app.get('/getDescList', function(req , res){
     var givenName = req.query.givenName;
     var surname = req.query.surname;
     var numGen = req.query.numGen;
-    console.log("given name: " + givenName + ", surname: " + surname + ", numGen" + numGen);
+    console.log("given name: " + givenName + ", surname: " + surname + ", numGen: " + numGen);
     var jsonString = parserLib.descToJSON(filePath, givenName, surname, numGen);
     console.log("jsonString = " + jsonString);
     var jsonObject = JSON.parse(jsonString);
