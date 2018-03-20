37f99bb04c5134b82f15517c1239afe264497cad
vicmoh <vimoh1213@gmail.com>
2018-03-19 22:28:37 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -184,7 +184,7 @@ $(document).ready(function() {
             $(".addIndiLastname").val(emptyString);
             // $('#addIndiSex').val(emptyString);
             // $('#addIndiFamSize').val(emptyString);
-            appendStringToStatus("Adding " + givenName + " " + surname + " to the list of individual in " + fileSelected.length + ".");
+            appendStringToStatus("Adding " + givenName + " " + surname + " to the list of individual in " + fileSelected + ".");
 
             //refresh the indi table
             console.log("calling ajax selection menu");
