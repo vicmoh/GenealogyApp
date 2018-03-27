1d837a8143bbfb065a50bb644a7a427ff8e69b2f
vicmoh <vimoh1213@gmail.com>
2018-03-27 17:56:34 -0400

    asdasd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -420,16 +420,16 @@ $(document).ready(function() {
     //login
     $('#loginID').click(function(){
         console.log("calling loginID jquery");
-        var User = $('#userID').val();
-        var Pass = $('#passID').val();
-        var Dbase = $('#dbaseID').val();
+        var user = $('#userID').val();
+        var pass = $('#passID').val();
+        var dbase = $('#dbaseID').val();
         console.log("user = " + user + ", passID = " + pass + ", dbase = " + dbase);
         
         $.ajax({
             type: 'get',
             dataType: 'json',
             url: '/login',
-            data: {user: User, pass: Pass, dbase: Dbase},
+            data: {user: user, pass: pass, dbase: dbase},
             success: function (data) {
                 console.log(data);
             },
