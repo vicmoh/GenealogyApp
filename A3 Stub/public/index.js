1115dc612968722e631d72871e6f26966fb709ed
vicmoh <vimoh1213@gmail.com>
2018-03-29 14:49:43 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -548,7 +548,7 @@ function appendToTable(data, toBeAppend){
         console.log("values = " + values);
         for(var y=0; y<values.length; y++){
             var bodyList = bodyList + "<th>" + values[y] + "</th>";
-            body = "<tbody><tr>"+ bodyList + "</tr></tbody>";
+            body =+ "<tbody><tr>"+ bodyList + "</tr></tbody>";
         }//end for
     }//end for
     $(toBeAppend).append(body);
