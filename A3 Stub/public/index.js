59cede403ecaf9eef4b32c67d541370ecf7d2c07
vicmoh <vimoh1213@gmail.com>
2018-03-29 20:43:03 -0400

    asd

--- A3 Stub/public/index.js
+++ A3 Stub/public/index.js
@@ -525,7 +525,7 @@ $(document).ready(function() {
             var command = "SELECT * FROM INDIVIDUAL ORDER BY surname;";
             $(jqueryID).val(command);
         }else if(fileSelected == "Get the individuals from a specific file"){
-            window.location = document.getElementById('queryPopupID').href;
+            window.location = document.getElementById('sq2').href;
             var fileName;
             var command = "SELECT * FROM INDIVIDUAL WHERE source_file = (SELECT file_id FROM FILE WHERE file_Name = \"shakespeare.ged\");";
             $(jqueryID).val(command);
